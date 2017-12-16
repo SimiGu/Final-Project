52360f924b6eee0ec2014b0b5a416720ba228cda
SimiGu <31868735+SimiGu@users.noreply.github.com>
2017-12-16 18:22:34 -0500

    push 2 right file

--- sketch.js
+++ sketch.js
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
 var ball1,ball2;
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-var ball1,ball2;
-=======
-var ball;
->>>>>>> 5df3ebfc9aae0be76001dc152aa9b14515284500
-=======
-var ball;
->>>>>>> 5df3ebfc9aae0be76001dc152aa9b14515284500
->>>>>>> 306c3e0f44cc21b3f81517b9097d7a92f52e73fb
 var otherballs;
 var platform;
 var ladder;
 var gravity=0.5;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 306c3e0f44cc21b3f81517b9097d7a92f52e73fb
 var scene=1;
 var posx=500;
 var posy;
@@ -104,56 +87,10 @@ function setup(){
 	// 	other.mass=other.scale;
 	// 	otherballs.add(other);
 	// }
-<<<<<<< HEAD
 }
 
 function draw(){
-=======
-=======
-=======
->>>>>>> 5df3ebfc9aae0be76001dc152aa9b14515284500
 
-
-function setup(){
-	createCanvas(800,450);
-
-	platform=createSprite(400,350);
-	platform.addImage(loadImage("assets/platform.png"));
-	platform.setCollider("rectangle",0,130,800,191);
-	//platform.debug=true;
-
-    ladder=createSprite(400,-196);
-    ladder.addImage(loadImage("assets/ladder.png"));
-    ladder.setCollider("rectangle",-20,-170,150,15);
-    //ladder.debug=true;
-
-	ball=createSprite(400,300);
-	ball.addAnimation("normal","assets/ball_normal0001.png","assets/ball_normal0004.png");
-	ball.addAnimation("fall","assets/ball_ground0001.png","assets/ball_ground0004.png");
-	ball.setCollider("circle",0,0,75);
-	//ball.debug=true;
-
-	otherballs=new Group();
-	for(var i=0;i<20;i++){
-		var other=createSprite(random(width),random(-200,-75));
-		other.addAnimation("normal","assets/ball_normal0001.png","assets/ball_normal0004.png");
-		other.setCollider("circle",0,0,75);
-		other.setSpeed(random(2,3), random(0, 360));
-		other.scale=random(0.3,1);
-		other.mass=other.scale;
-		otherballs.add(other);
-	}
-<<<<<<< HEAD
->>>>>>> 5df3ebfc9aae0be76001dc152aa9b14515284500
-=======
->>>>>>> 5df3ebfc9aae0be76001dc152aa9b14515284500
-
-}
-
-function draw(){
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 306c3e0f44cc21b3f81517b9097d7a92f52e73fb
 //start from dark
 	if(frameCount<100){
 		background(0);	
@@ -323,87 +260,4 @@ if(ball2.overlapPixel(tie.position.x,tie.position.y-60)){
 //test scenes
 function mousePressed(){
 	scene++;
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> 5df3ebfc9aae0be76001dc152aa9b14515284500
-	background(255);
-
-	ball.velocity.y+=gravity;
-	if(ball.collide(platform)){
-		ball.velocity.y=0;
-	}
-
-	if(keyWentDown("space")){
-		ball.velocity.y=-4;
-	}
-	if(keyIsDown(LEFT_ARROW))
-		ball.position.x-=5;
-	if(keyIsDown(RIGHT_ARROW))
-		ball.position.x+=5;
-
-
-	if(frameCount<400){
-		ball.position.y=constrain(ball.position.y,280,400);
-	}
-
-	if(frameCount>300){
-		ladder.position.y+=2;
-		ladder.position.y=constrain(ladder.position.y,-196,196);
-	}
-	ball.collide(ladder);
-
-	if(ball.position.y<250&&ball.position.y>180){
-		ladder.setCollider("rectangle",20,115,80,15);
-	}
-	if(ball.position.y<180&&ball.position.y>100){
-		ladder.setCollider("rectangle",10,40,80,15);
-	}
-	if(ball.position.y<100&&ball.position.y>20){
-		ladder.setCollider("rectangle",0,-30,80,15);
-	}
-    if(ball.position.y<20&&ball.position.y>0){
-		ladder.setCollider("rectangle",-10,-100,80,15);
-	}
-	if(ball.position.y<-60){
-		ladder.setCollider("rectangle",-20,-170,80,15);
-	}
-	
-	//ladder.debug=true;
-	
-	
-if(ball.position.y<-50){
-	camera.zoom=0.5;
-}else{
-	camera.zoom=1;
-}
-
-otherballs.bounce(otherballs);
-
-for(var i=0;i<otherballs.length;i++){
-var s=otherballs[i];
-
-  if(s.position.x<0) {
-    s.position.x = 1;
-    s.velocity.x = abs(s.velocity.x);
-  }
-  
-  if(s.position.x>width) {
-    s.position.x = width-1;
-    s.velocity.x = -abs(s.velocity.x);
-    }
-
-  if(s.position.y>-75) {
-    s.position.y = -76;
-    s.velocity.y = abs(s.velocity.y);
-  }
-  }
-
- drawSprites();
-<<<<<<< HEAD
->>>>>>> 5df3ebfc9aae0be76001dc152aa9b14515284500
-=======
->>>>>>> 5df3ebfc9aae0be76001dc152aa9b14515284500
->>>>>>> 306c3e0f44cc21b3f81517b9097d7a92f52e73fb
 }
